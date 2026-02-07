d1f652c8680bf561cf7ab01f524e859b4e9eafd3
Kayos <tuankhoi0206@gmail.com>
2026-02-07 15:30:36 -0500

    Change them to not clash with gitignore

--- louis/src/core/paths.py
+++ louis/src/core/paths.py
@@ -8,7 +8,7 @@ def get_project_root() -> Path:
 
 
 def get_data_dir() -> Path:
-    return get_project_root() / "data"
+    return get_project_root() / "datasets"
 
 
 def get_processed_dir() -> Path:
@@ -17,4 +17,3 @@ def get_processed_dir() -> Path:
 
 def get_runs_dir() -> Path:
     return get_project_root() / "runs"
-
